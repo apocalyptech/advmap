3bfaf53e2ffb2c1166bb41d4e1f2a9ba99f0a26d
CJ Kucera <pez@apocalyptech.com>
2017-09-18 10:47:56 -0500

    A bit more work trying to get our QMessageBox to resize itself a little wider, without success

--- advmap/gui.py
+++ advmap/gui.py
@@ -648,6 +648,11 @@ class GUI(QtWidgets.QMainWindow):
         msgbox.setDefaultButton(default_button)
         msgbox.setIcon(icon)
         msgbox.setWindowTitle(message)
+        msgbox.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
+        # TODO: How the hell can we make this dialog larger?  These, below,
+        # do nothing, basically:
+        #msgbox.setSizeGripEnabled(True)
+        #msgbox.setMinimumWidth(600)
         return msgbox.exec()
 
     def dialog_confirm(self, message, infotext=None):
