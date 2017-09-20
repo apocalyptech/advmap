8a09afc5f04a9830317f7ac466b225bd8b038678
CJ Kucera <pez@apocalyptech.com>
2017-09-20 10:23:01 -0500

    Rich Text / HTML inside QComboBoxes and QCheckboxes, to match how the Gtk version used to look

--- advmap/gui.py
+++ advmap/gui.py
@@ -248,6 +248,200 @@ class Constants(object):
     # pretty terribly, but I do not feel bad about this.
     statusbar = None
 
+class HTMLStyle(QtWidgets.QProxyStyle):
+    """
+    A QProxyStyle which can be used to render HTML/Rich text inside
+    QComboBoxes and QCheckBoxes.  Note that for QComboBox, this does
+    NOT alter rendering of the items when you're choosing from the
+    list.  For that you'll need to set an item delegate.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_doc = QtGui.QTextDocument()
+
+    def drawItemText(self, painter, rect, alignment, pal, enabled, text, text_role):
+        """
+        This is what draws the text - we use an internal QTextDocument
+        to do the formatting.  The general form of this function follows the
+        C++ version at https://github.com/qt/qtbase/blob/5.9/src/widgets/styles/qstyle.cpp
+
+        Note that we completely ignore the `alignment` and `enabled` variable.
+        This is always left-aligned, and does not currently support disabled
+        widgets.
+        """
+        if not text or text == '':
+            return
+
+        # Save our current pen if we need to
+        saved_pen = None
+        if text_role != QtGui.QPalette.NoRole:
+            saved_pen = painter.pen()
+            painter.setPen(QtGui.QPen(pal.brush(text_role), saved_pen.widthF()))
+
+        # Render the text.  There's a bit of voodoo here with the rectangles
+        # and painter translation; there was various bits of finagling necessary
+        # to get this to seem to work with both combo boxes and checkboxes.
+        # There's probably better ways to be doing this.
+        margin = 3
+        painter.save()
+        painter.translate(rect.left()-margin, 0)
+        self.text_doc.setHtml(text)
+        self.text_doc.drawContents(painter,
+                QtCore.QRectF(rect.adjusted(-rect.left(), 0, -margin, 0)))
+        painter.restore()
+
+        # Restore our previous pen if we need to
+        if text_role != QtGui.QPalette.NoRole:
+            painter.setPen(saved_pen)
+
+    def sizeFromContents(self, contents_type, option, size, widget=None):
+        """
+        For ComboBoxes, this gets called to determine the size of the list of
+        options for the comboboxes.  This is too wide for our HTMLComboBox, so
+        we pull in the width from there instead.
+        """
+        width = size.width()
+        height = size.height()
+        if contents_type == self.CT_ComboBox and widget and type(widget) == HTMLComboBox:
+            size = widget.sizeHint()
+            width = size.width() + widget.width_adjust_contents
+        return super().sizeFromContents(contents_type,
+                option,
+                QtCore.QSize(width, height),
+                widget)
+
+class HTMLDelegate(QtWidgets.QStyledItemDelegate):
+    """
+    Class for use in a QComboBox to allow HTML Text.  I'm still a bit
+    miffed that this isn't just a default part of Qt.  There's a lot of
+    Google hits from people looking to do this, most suggesting
+    implementing something like this, but so far I've only found this
+    one actual implementation, at the end of a thread here:
+    http://www.qtcentre.org/threads/62867-HTML-rich-text-delegate-and-text-centering-aligning-code-amp-pictures
+
+    I suspect this implementation is probably heavier than we actually
+    need, but it seems fairly voodooey anyway.  And keep in mind that
+    after all this, you've still got to produce a completely bloody
+    different solution for displaying the currently-selected item in
+    the QComboBox; this is only for the list of choices.  I'm happy
+    to be leaving Gtk but this kind of thing makes the move more
+    bittersweet than it should be.
+    """
+
+    def __init__(self, parent=None):
+        super().__init__()
+        self.doc = QtGui.QTextDocument(self)
+
+    def paint(self, painter, option, index):
+        """
+        Paint routine for our items in the QComboBox
+        """
+
+        # Save our painter so it can be restored later
+        painter.save()
+
+        # Copy our option var so we can make some changes without modifying
+        # the underlying object
+        options = QtWidgets.QStyleOptionViewItem(option)
+        self.initStyleOption(options, index)
+
+        # Add in our data to our QTextDocument
+        self.doc.setHtml(options.text)
+
+        # Acquire our style
+        if options.widget is None:
+            style = QtWidgets.QApplication.style()
+        else:
+            style = options.widget.style()
+
+        # Draw a barebones version of the control which doesn't have any
+        # text specified - this is to render the background, basically, so
+        # that when we're mousing over one of the items the bg changes.
+        options.text = ''
+        style.drawControl(QtWidgets.QStyle.CE_ItemViewItem, options, painter)
+
+        # Grab a PaintContext and set our text color depending on if we're
+        # selected or not
+        ctx = QtGui.QAbstractTextDocumentLayout.PaintContext()
+        if option.state & QtWidgets.QStyle.State_Selected:
+            ctx.palette.setColor(QtGui.QPalette.Text, option.palette.color(
+                QtGui.QPalette.Active, QtGui.QPalette.HighlightedText))
+        else:
+            ctx.palette.setColor(QtGui.QPalette.Text, option.palette.color(
+                QtGui.QPalette.Active, QtGui.QPalette.Text))
+
+        # Calculating some rendering geometry.
+        textRect = style.subElementRect(
+            QtWidgets.QStyle.SE_ItemViewItemText, options)
+        textRect.adjust(3, 0, 0, 0)
+        painter.translate(textRect.topLeft())
+        painter.setClipRect(textRect.translated(-textRect.topLeft()))
+
+        # Now, finally, actually render the text
+        self.doc.documentLayout().draw(painter, ctx)
+
+        # Restore our paintbrush
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        """
+        Our size.  This actually gets called before `paint`, I think, and therefore
+        is called before our text has actually been loaded into the QTextDocument,
+        but apparently seems to Do The Right Thing Anyway.
+        """
+        return QtCore.QSize(self.doc.idealWidth(), self.doc.size().height())
+
+class HTMLComboBox(QtWidgets.QComboBox):
+    """
+    Custom QComboBox class to handle dealing with HTML/Rich text.  This
+    is basically just here to set a few attributes and then implement
+    a custom sizeHint.  This implementation does NOT support ComboBoxes
+    whose contents get updated - sizeHint will cache its information
+    the first time it's called and then never update it.
+    """
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setStyle(HTMLStyle())
+        self.setItemDelegate(HTMLDelegate())
+        self.stored_size = None
+
+        # TODO: Figure out how to actually calculate these properly
+        self.width_adjust_sizehint = 20
+        self.width_adjust_contents = -30
+
+    def sizeHint(self):
+        """
+        Use a QTextDocument to compute our rendered text size
+        """
+        if not self.stored_size:
+            doc = QtGui.QTextDocument()
+            model = self.model()
+            max_w = 0
+            max_h = 0
+            for rownum in range(model.rowCount()):
+                item = model.item(rownum)
+                doc.setHtml(item.text())
+                size = doc.size()
+                if size.width() > max_w:
+                    max_w = size.width()
+                if size.height() > max_h:
+                    max_h = size.height()
+
+            # Need to add in a bit of padding to account for the
+            # arrow selector
+            max_w += self.width_adjust_sizehint
+
+            self.stored_size = QtCore.QSize(max_w, max_h)
+        return self.stored_size
+
+    def minimumSizeHint(self):
+        """
+        Just use the same logic as `sizeHint`
+        """
+        return self.sizeHint()
+
 class FirstLineEdit(QtWidgets.QLineEdit):
     """
     Stupid little class which automatically sets the cursor position
@@ -3322,11 +3516,13 @@ class NewEditRoomDialog(AppDialog):
 
         # Width options
         self.add_label('Width Options')
-        self.input_offset_x = self.add_checkbox('Offset (shift to the right)')
+        self.input_offset_x = self.add_checkbox('Offset <i>(shift to the right)</i>')
+        self.input_offset_x.setStyle(HTMLStyle())
 
         # Height options
         self.add_label('Height Options')
-        self.input_offset_y = self.add_checkbox('Offset (shift down)')
+        self.input_offset_y = self.add_checkbox('Offset <i>(shift down)</i>')
+        self.input_offset_y.setStyle(HTMLStyle())
 
         # Grouping widget choice depends on what exactly we're doing
         if not self.editing and self.from_direction is not None:
@@ -3569,23 +3765,8 @@ class NewEditRoomDialog(AppDialog):
         Adds a group-with dropdown.  This will omit any rooms already in
         a different group than us.
         """
-        # TODO: Hrmph.  I'm miffed that Qt doesn't actually support rich/html text
-        # inside QComboBoxes (or QCheckBoxes, for that matter).  There's some ways to
-        # hack it in, though for now it's defeated me.  One way is to set up a
-        # QAbstractItemDelegate class, overriding paint() and sizeHint(), and use
-        # an internal QTextDocument there to paint the dropdown selection.  That's
-        # not too bad, but you've also got to render the mouseovers, which I've not
-        # investigated how to do, and there were some strange rendering glitches I
-        # hadn't sorted out, and that doesn't actaully alter how it shows up once you
-        # *have* selected an item (ie: the combobox then just shows the HTML tags).
-        # Another post at here: https://stackoverflow.com/questions/22435712/how-to-display-superscript-in-qcombobox-item
-        # suggests that you may be able to override the style of QComboBox using
-        # a QProxyStyle class, but as with all the other suggestions as well, I've
-        # never actually seen a working example anywhere.  I suspect that very few
-        # people, if any, have actually tried to implement this and succeeded.
-        # Anyway, for now I'm just going to deal with plaintext and sulk.
         scene = self.parent().scene
-        cb = QtWidgets.QComboBox(self)
+        cb = HTMLComboBox(self)
         cb.addItem('-', None)
         cur_group = None
         if self.room:
@@ -3595,7 +3776,7 @@ class NewEditRoomDialog(AppDialog):
                 continue
             if room.group and cur_group and room.group != cur_group:
                 continue
-            cb.addItem('{}, at ({}, {})'.format(room.name, room.x+1, room.y+1), room)
+            cb.addItem('<b>{}</b><i> at ({}, {})</i>'.format(room.name, room.x+1, room.y+1), room)
         self.gridlayout.addWidget(cb, self.cur_row, 1, QtCore.Qt.AlignLeft)
         return cb
 
